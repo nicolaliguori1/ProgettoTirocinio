ccc7b6d4702d64ab9d1e932d345b7a48c05d21e9
Nicola <n.liguori1@studenti.unisa.it>
2025-06-21 13:00:39 +0200

    Spostati file nella cartella src

--- README.md
+++
@@ -1,2 +0,0 @@
-# TrackerBoat
-Creazione di un sistema di monitoraggio dei natanti in ingresso e in uscita da un molo.
---
+++ TrackingBarca/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import TrackerBarca from './TrackerBarca';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <TrackerBarca />
+    </div>
+  );
+}
+
+export default App;
+
