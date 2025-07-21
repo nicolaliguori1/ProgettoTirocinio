65e963fd1a2a2a88477d00aa90e1ca0c45bbf109
Nicola <n.liguori1@studenti.unisa.it>
2025-07-21 23:18:44 +0200

    aggiunyo login css e modificata referenza

--- login.php
+++ login.php
@@ -36,7 +36,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="login.css?v=2">
     <title>Login</title>
 </head>
 
