d793765dc499b68a145b7e1a4c55c96992481ff5
Nicola <n.liguori1@studenti.unisa.it>
2025-07-23 16:49:02 +0200

    database aggiornato

--- database.sql
+++ database.sql
@@ -5,14 +5,14 @@ CREATE EXTENSION IF NOT EXISTS pgcrypto;
 -- Creazione delle tabelle
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
-  nome_utente VARCHAR(20) NOT NULL,
+  nome_utente VARCHAR(100) NOT NULL,
   pw VARCHAR(100) NOT NULL,
-  email VARCHAR(50) NOT NULL
+  email VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE fari (
   id SERIAL PRIMARY KEY,
-  nome VARCHAR(20) NOT NULL,
+  nome VARCHAR(100) NOT NULL,
   lat DOUBLE PRECISION NOT NULL,
   lon DOUBLE PRECISION NOT NULL
 );
@@ -30,9 +30,9 @@ CREATE TABLE fari_position (
 
 
 CREATE TABLE boats (
-  targa VARCHAR(20) PRIMARY KEY,
+  targa VARCHAR(100) PRIMARY KEY,
   lunghezza INT NOT NULL,
-  nome VARCHAR(20) NOT NULL,
+  nome VARCHAR(100) NOT NULL,
   id_user INT,
   id_faro INT,
   FOREIGN KEY (id_user) REFERENCES users(id)
@@ -45,7 +45,7 @@ CREATE TABLE boats (
 
 CREATE TABLE boats_position (
   ts TIMESTAMP DEFAULT NOW(),
-  targa_barca VARCHAR(20) NOT NULL,
+  targa_barca VARCHAR(100) NOT NULL,
   lat DOUBLE PRECISION NOT NULL,
   lon DOUBLE PRECISION NOT NULL,
   PRIMARY KEY (ts, targa_barca),
