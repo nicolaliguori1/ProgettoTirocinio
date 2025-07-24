d64d85e03d12a27f602910fdf120b126a99f7f61
Marco <m.orefice12@studenti.unisa.it>
2025-07-24 16:49:03 +0200

    ooo

--- database.sql
+++ database.sql
@@ -54,41 +54,22 @@ CREATE TABLE boats_position (
     ON UPDATE CASCADE
 );
 
+CREATE TABLE boats_current_position (
+  targa_barca VARCHAR(100) PRIMARY KEY,
+  lat DOUBLE PRECISION NOT NULL,
+  lon DOUBLE PRECISION NOT NULL,
+  ts TIMESTAMP NOT NULL,
+  FOREIGN KEY (targa_barca) REFERENCES boats(targa)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+
 -- Simulazione realistica
 INSERT INTO fari (nome, lat, lon) VALUES
 ('Faro di Capo d’Orso', 40.6333, 14.6833),
 ('Faro di Punta Licosa', 40.2500, 14.9000),
 ('Faro del Porto di Salerno (Molo Manfredi)', 40.6745, 14.7519);
-
--- Percorso dal Faro di Capo d’Orso (Costiera Amalfitana)
--- Capo d'Orso: 40.6333, 14.6833
-(40.6333, 14.6833),
-(40.6280, 14.7000),
-(40.6200, 14.7150),
-(40.6100, 14.7200), -- Verso Amalfi
-(40.6150, 14.7050),
-(40.6220, 14.6900),
-(40.6300, 14.6800) -- Ritorno
-
--- Percorso dal Faro di Punta Licosa (Castellabate)
--- Punta Licosa: 40.2500, 14.9000
-(40.2500, 14.9000),
-(40.2480, 14.9100),
-(40.2450, 14.9200),
-(40.2400, 14.9250), -- al largo
-(40.2420, 14.9150),
-(40.2460, 14.9050),
-(40.2500, 14.9000) -- Ritorno
-
---  Percorso dal Faro del Porto di Salerno (Molo Manfredi)
--- Molo Manfredi: 40.6745, 14.7519
-(40.6745, 14.7519),
-(40.6720, 14.7600),
-(40.6650, 14.7700),
-(40.6600, 14.7600), -- Verso Vietri
-(40.6630, 14.7450),
-(40.6680, 14.7400),
-(40.6745, 14.7519) -- Ritorno
 
 
 -- barche
@@ -96,3 +77,5 @@ INSERT INTO boats (targa, lunghezza, nome, id_user, id_faro) VALUES
 ('SA1234', 12, 'Aurora', NULL, 1),  -- Faro di Capo d’Orso
 ('SA5678', 9, 'Stella Marina', NULL, 2),  -- Faro di Punta Licosa
 ('SA9012', 15, 'Onda Libera', NULL, 3);  -- Faro del Porto di Salerno
+
+-- prova
